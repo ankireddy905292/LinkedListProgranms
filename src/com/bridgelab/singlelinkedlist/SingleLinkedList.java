444112fdaf87dfa0dd597f00929568ae4297b2f2
Tutorials-Git <ankireddy905292@gmail.com>
2021-11-17 06:06:49 +0530

    first LinkedList

---
+++ src/com/bridgelab/singlelinkedlist/SingleLinkedList.java
@@ -0,0 +1,7 @@
+package com.bridgelab.singlelinkedlist;
+
+public class SingleLinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome To LinkedList");
+    }
+}
