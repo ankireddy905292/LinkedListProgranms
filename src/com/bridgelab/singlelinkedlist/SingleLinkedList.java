903176910c2a997904bd67597ccc718f551d7730
Tutorials-Git <ankireddy905292@gmail.com>
2021-11-17 06:27:59 +0530

    first LinkedList

--- src/com/bridgelab/singlelinkedlist/SingleLinkedList.java
+++ src/com/bridgelab/singlelinkedlist/SingleLinkedList.java
@@ -1,7 +1,78 @@
 package com.bridgelab.singlelinkedlist;
-
+import java.util.*;
 public class SingleLinkedList {
-    public static void main(String[] args) {
-        System.out.println("Welcome To LinkedList");
+
+    //Represent a node of the singly linked list
+        public static class Node{
+            private int data;
+            private Node next;
+
+            public Node(int x){
+                this.data = x;
+                this.next = null;
+            }
+        }
+
+
+        //Represent the head and tail of the singly linked list
+        private Node head = null;
+        private Node tail = null;
+
+        //addNode() will add a new node to the list,
+        // Create a new node,
+        // Checks if the list is empty by using head address
+        public void addNode(int x){
+            Node newNode = new Node(x);
+
+            if (head == null){
+                head = newNode;
+            }else{
+                tail.next = newNode;
+                tail = newNode;
+            }
+            tail = newNode;
+        }
+
+        //displayList() will display all the nodes
+        public void displayList(){
+            Node temp = head;
+            if (head == null){
+                System.out.println("Singly Linked List is Empty");
+            }
+            System.out.println("LinkedList Sequence:");
+            while (temp != null){
+                System.out.println(temp.data + "");
+                temp = temp.next;
+            }
+            System.out.println();
+
+        }
+
+        //Add nodes to the list at Beginning
+        public void addToBegin(int x){
+            if (head == null){
+                this.head = new Node(x);
+            }else{
+                Node temp = new Node(x);
+                temp.next = head;
+                head = temp;
+            }
+        }
+
+
+
+        public static void main(String[] args) {
+            SingleLinkedList slList = new SingleLinkedList();
+
+            //Add nodes to the list at Beginning
+            slList.addToBegin(70);
+            slList.addToBegin(30);
+            slList.addToBegin(56);
+
+
+            //Call the Displays Method
+            slList.displayList();
+        }
     }
-}
+
+
